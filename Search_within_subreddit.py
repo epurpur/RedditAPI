3408452c707bfdcc8870981bc9863ea4a8194528
Erich Purpur <epurpur@gmail.com>
2021-10-11 14:33:32 -0400

    Add files via upload

--- Search_within_subreddit.py
+++ Search_within_subreddit.py
@@ -4,12 +4,16 @@ The following script searches the Charlottesville subreddit (r/Charlottesville)
 The first several lines set up Authentication. The 'Passwords' module is not a part of the github repo and a user must
 create their own reddit client_id and secret_key. To set this up I recommend the following link and youtube video: https://towardsdatascience.com/how-to-use-the-reddit-api-in-python-5e05ddfd1e5c
 
-Once authentication is set up, the code searches for posts in the Charlottesville subreddit with the search text of 'food'. The response is a lot of JSON data but in there is all information about the post. 
+Once authentication is set up, the code searches for new posts in the Charlottesville subreddit. The response is a lot of JSON data but in there is all information about the post. 
 From there it makes a list of each post's id and makes a request for each post using that ID. 
 The final output is formatted JSON of the title of the post followed by the text of each comment. 
 
 Reddit API documentation: https://www.reddit.com/dev/api/
+
 """
+
+
+
 
 import requests
 import pandas as pd
@@ -46,9 +50,10 @@ print(requests.get('https://oauth.reddit.com/api/v1/me', headers=headers))
 
 
 
-#### GET NEW POSTS FROM SUBREDDIT
+#### GET NEW POSTS FROM SUBREDDIT WITH SEARCH TERM 'food'
 
-res = requests.get('https://oauth.reddit.com/r/Charlottesville/new',
+#restrict_sr is required or else results are not limited to search term
+res = requests.get('https://oauth.reddit.com/r/Charlottesville/search.json?q=food&restrict_sr=on',
                    headers=headers,
                    params={'limit': '5'})    # top 5 posts
 
@@ -57,12 +62,12 @@ df = pd.DataFrame()
 
 for post in res.json()['data']['children']:
     df = df.append({'subreddit': post['data']['subreddit'],
-               'title': post['data']['title'],
-               'selftext': post['data']['selftext'],
-               'post_id': post['data']['id']
-               }, ignore_index=True)
+                'title': post['data']['title'],
+                'selftext': post['data']['selftext'],
+                'post_id': post['data']['id']
+                }, ignore_index=True)
     
-post_ids = df['post_id'].tolist()
+post_ids = df['post_id'].tolist()           # makes list of post ids
 
 
 
@@ -71,7 +76,10 @@ for post in post_ids:
     res = requests.get(f'https://oauth.reddit.com/r/Charlottesville/comments/{post}', headers=headers)
     
     data = res.json()
-
+    
+    print()    
+    print('***NEXT POST***')
+    
     #post content
     print(data[0]['data']['children'][0]['data']['title'])
     print(data[0]['data']['children'][0]['data']['selftext'])
@@ -81,6 +89,5 @@ for post in post_ids:
         print('COMMENT---')
         print(comment['data']['body'])
         
-    print()    
-    print('***NEXT POST***')
+    
     print()
